7ffe6dc523739dfa3d511654beacb0f268787f13
Abdullah <imuhammadabdullah@outlook.com>
2025-10-20 22:06:18 +0500

    tesgt

--- index.js
+++ index.js
@@ -502,7 +502,7 @@ ensureDatabaseExists()
       console.log("Inserted default products.");
     }
     app.listen(port, () => {
-      console.log("test new log");
+      console.log("test new log1");
       console.log(`Server is running on http://localhost:${port}`);
     });
   })
